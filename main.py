9ebd62e9ea78ce0e1e50e66176ceaff23bc7722f
Fede.Breg <44495483+Thepowa753@users.noreply.github.com>
2022-06-09 10:29:57 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from subprocess import Popen, check_output
+from os.path import isfile, expanduser
+from os import system
+
+
+get_version_string = r"""curl --silent "https://api.github.com/repos/dbeaver/dbeaver/releases/latest"  | grep "tag_name" | sed -E 's/.*"([^"]+)".*/\1/'"""
+download_latest_string = r"""wget -O ~/.local/dbeaver-ce-latest-stable.x86_64.rpm https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm"""
+chmodding_string = r"chmod 777 ~/.local/dbeaver-ce-latest-stable.x86_64.rpm"
+install_latest_string = r"""sudo dnf install -y ~/.local/dbeaver-ce-latest-stable.x86_64.rpm && rm ~/.local/dbeaver-ce-latest-stable.x86_64.rpm"""
+latest_installed_path = expanduser("~/.config/dbeaver_version")
+
+if not isfile(latest_installed_path):
+	with open(latest_installed_path, "w") as f:
+		f.write("\n")
+
+latest_version = str(check_output(get_version_string, shell=True), "utf-8")
+print(f"Latest version from git: {latest_version}")
+with open(latest_installed_path, "r") as f:
+	if latest_version in f.readline():
+		print("Already up to date!")
+		exit(0)
+check_output(download_latest_string, shell=True)
+check_output(chmodding_string, shell=True)
+if system(install_latest_string) == 0:
+	with open(latest_installed_path, "w") as f:
+		f.write(f"{latest_version}\n")
+else:
+	print("Errore installazione")
