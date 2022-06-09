02ccf611dd67f2ef7718ca19bf43dd49f3280430
Fede.Breg <44495483+Thepowa753@users.noreply.github.com>
2022-06-09 11:01:04 +0200

    feat: supporting deb

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 from subprocess import check_output
 from os.path import isfile, expanduser
 from os import system
+from distro import id as linux_type
 
+
+linux = linux_type().lower().replace(" ", "")
+match linux:
+	case "fedora":
+		download_latest_string = r"""wget -O ~/.local/dbeaver-ce-latest-stable.x86_64.rpm https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm"""
+		chmodding_string = r"chmod 777 ~/.local/dbeaver-ce-latest-stable.x86_64.rpm"
+		install_latest_string = r"""sudo dnf install -y ~/.local/dbeaver-ce-latest-stable.x86_64.rpm && rm ~/.local/dbeaver-ce-latest-stable.x86_64.rpm"""
+	case "redhat":
+		download_latest_string = r"""wget -O ~/.local/dbeaver-ce-latest-stable.x86_64.rpm https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm"""
+		chmodding_string = r"chmod 777 ~/.local/dbeaver-ce-latest-stable.x86_64.rpm"
+		install_latest_string = r"""sudo dnf install -y ~/.local/dbeaver-ce-latest-stable.x86_64.rpm && rm ~/.local/dbeaver-ce-latest-stable.x86_64.rpm"""
+	case "ubuntu":
+		download_latest_string = r"""wget -O ~/.local/dbeaver-ce-latest-stable.x86_64.deb https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.deb"""
+		chmodding_string = r"chmod 777 ~/.local/dbeaver-ce-latest-stable.x86_64.deb"
+		install_latest_string = r"""sudo apt install -y ~/.local/dbeaver-ce-latest-stable.x86_64.deb && rm ~/.local/dbeaver-ce-latest-stable.x86_64.deb"""
+	case "debian":
+		download_latest_string = r"""wget -O ~/.local/dbeaver-ce-latest-stable.x86_64.deb https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.deb"""
+		chmodding_string = r"chmod 777 ~/.local/dbeaver-ce-latest-stable.x86_64.deb"
+		install_latest_string = r"""sudo apt install -y ~/.local/dbeaver-ce-latest-stable.x86_64.deb && rm ~/.local/dbeaver-ce-latest-stable.x86_64.deb"""
+	case "kali":
+		download_latest_string = r"""wget -O ~/.local/dbeaver-ce-latest-stable.x86_64.deb https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.deb"""
+		chmodding_string = r"chmod 777 ~/.local/dbeaver-ce-latest-stable.x86_64.deb"
+		install_latest_string = r"""sudo apt install -y ~/.local/dbeaver-ce-latest-stable.x86_64.deb && rm ~/.local/dbeaver-ce-latest-stable.x86_64.deb"""
+	case _:
+		print(f"Wait!\n{linux} distro not supported...\nPlease write an issue!")
+		exit(5)
 
 get_version_string = r"""curl --silent "https://api.github.com/repos/dbeaver/dbeaver/releases/latest"  | grep "tag_name" | sed -E 's/.*"([^"]+)".*/\1/'"""
-download_latest_string = r"""wget -O ~/.local/dbeaver-ce-latest-stable.x86_64.rpm https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm"""
-chmodding_string = r"chmod 777 ~/.local/dbeaver-ce-latest-stable.x86_64.rpm"
-install_latest_string = r"""sudo dnf install -y ~/.local/dbeaver-ce-latest-stable.x86_64.rpm && rm ~/.local/dbeaver-ce-latest-stable.x86_64.rpm"""
 latest_installed_path = expanduser("~/.config/dbeaver_version")
 
 if not isfile(latest_installed_path):
